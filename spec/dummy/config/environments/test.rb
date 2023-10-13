8b135d1ff4048a3128bdd46338dd00f268d01fc8
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2023-10-13 23:34:28 +0900

    Enable reloading for test on rails 6.0

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -2,3 +2,7 @@ Rails.application.configure do
   config.cache_classes = true
   config.eager_load = false
 end
+
+if Rails::VERSION::STRING.to_f == 6.0
+  Rails.autoloaders.main.enable_reloading
+end
